366eb2d119a1a09bf97088ba28ed5c8a27d2352d
Reem Eldahlish <reem.essam.eldin@gmail.com>
2024-02-12 15:22:47 +0100

    still working

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,12 @@
 #include "ft_printf.h"
+#include "libft/libft.h"
 
 int	ft_printf(const char format, ...)
 {
+	va_list	args;
 
+	va_start(args, format);
+
+	va_end(args);
+	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,6 +1,7 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdio.h>
 # include <unistd.h>
 # include <stdarg.h>
 
