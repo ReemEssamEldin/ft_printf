28819e459deffdb8cc0625c4f35254613eca0185
Reem Eldahlish <reem.essam.eldin@gmail.com>
2024-02-12 12:45:16 +0100

    just start

---
+++ ft_printf.h
@@ -0,0 +1,9 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("The actual function: %d\n", 12);
+	return (0);
+}
